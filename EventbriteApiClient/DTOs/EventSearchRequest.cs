6e4cc8e89d699efc88ef25da947f46d3b5cb018a
Ivailo Zhelev <Ivailo.zhelev@execview.co.uk>
2014-10-28 22:48:29 +0000

    Do not include Start and and date ranges if they are not specified in the request

--- EventbriteApiClient/DTOs/EventSearchRequest.cs
+++ EventbriteApiClient/DTOs/EventSearchRequest.cs
@@ -19,11 +19,17 @@ namespace EventbriteApiClient.DTOs
             var request = new RestRequest("/events/search/?token={token}", Method.GET);
             request.AddParameter("location.latitude", this.Latitude.ToString());
             request.AddParameter("location.longitude", this.Longitude.ToString());
-            request.AddParameter("location.within", this.Range);        
-            request.AddParameter("start_date.range_start", this.DateRangeStart.ToUniversalTime()
-                       .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"));
-            request.AddParameter("start_date.range_end", this.DateRangeEnd.ToUniversalTime()
-                         .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"));
+            request.AddParameter("location.within", this.Range);
+            if (this.DateRangeStart != DateTime.MinValue)
+            {
+                request.AddParameter("start_date.range_start", this.DateRangeStart.ToUniversalTime()
+                    .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"));
+            }
+            if (this.DateRangeEnd != DateTime.MinValue)
+            {
+                request.AddParameter("start_date.range_end", this.DateRangeEnd.ToUniversalTime()
+                    .ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"));
+            }
 
             return request;
         }
