0f45f791cb308e266744fe85c9a3fb837aa5deca
Ivailo Zhelev <Ivailo.zhelev@execview.co.uk>
2014-10-28 22:22:28 +0000

    Added a few more properties to the Event entity

--- EventbriteApiClient/Entities/Event.cs
+++ EventbriteApiClient/Entities/Event.cs
@@ -4,10 +4,11 @@ namespace EventbriteApiClient.Entities
 {
     public class Event
     {
+        public string Id { get; set; }
         [DeserializeAs(Name = "resource_uri")]
         public string ResourceUri { get; set; }
         public MultipartText Name { get; set; }
-        public string Url { get;set; }
+        public string Url { get;set; }      
         [DeserializeAs(Name = "logo_url")]
         public string LogoUrl { get; set; }
         public DateTimeTz Start { get; set; }
